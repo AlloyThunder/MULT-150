79684ef82769ba4dd0d767663e483633b1f5626a
AlloyThunder <sammyjmalo@gmail.com>
2026-03-14 00:26:41 -0700

    your script is missing a ";"

---
+++ SMALO_HOUR7_P1/Assets/scripts/HelloWorldScript.cs
@@ -0,0 +1,16 @@
+using Unity.VisualScripting.FullSerializer;
+using UnityEngine;
+public class HelloWorldScript : MonoBehaviour
+{
+    public int runSpeed;
+    // Use this for initialization
+    void Start ()
+    {
+        print("Hello World");
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
